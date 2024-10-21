18c9cda8016368829283c5b2eebd72fec610970e
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-10-21 21:29:41 +0530

    sign-in and up pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -7,6 +11,8 @@ interface Props {
 }
 
 const AuthLayout = ({ children }: Props) => {
+  const pathname = usePathname();
+
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
@@ -15,7 +21,11 @@ const AuthLayout = ({ children }: Props) => {
             <Image src="/logo.svg" width={50} height={55} alt="Flowz" />
             <h1 className="font-semibold text-xl select-none">Flowz</h1>
           </div>
-          <Button variant="secondary">Sign Up</Button>
+          <Button asChild variant="secondary">
+            <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
+              {pathname === "/sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
           {children}
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
 import React from "react";
 
 const page = () => {
-  return <div>sign-up</div>;
+  return <SignUpCard />;
 };
 
 export default page;
