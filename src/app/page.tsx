25c418978f406f3f107c6275eaba772ea9e2e536
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-10-24 20:17:28 +0530

    user avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data]);
-
   return (
     <div>
-      Only visible to authorized users
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
