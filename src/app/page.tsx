556e3fc63f9574fa55862f483d4f6bc04056084a
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-10-24 15:56:33 +0530

    middleware integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <Button variant="teritary">Click</Button>
-    </div>
-  );
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  return <div>Only visible to authorized users</div>;
 }
