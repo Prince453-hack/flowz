a8ae8c064d5944212f985eb0b227c9e3b31bab2a
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-10-24 18:12:00 +0530

    logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
 
   useEffect(() => {
     if (!data && !isLoading) {
@@ -14,5 +17,10 @@ export default function Home() {
     }
   }, [data]);
 
-  return <div>Only visible to authorized users</div>;
+  return (
+    <div>
+      Only visible to authorized users
+      <Button onClick={() => mutate()}>Logout</Button>
+    </div>
+  );
 }
--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,15 +1,20 @@
 import { client } from "@/lib/rpc";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
-type ResponseType = InferResponseType<(typeof client.api.auth.login)["$post"]>;
-type RequestType = InferRequestType<(typeof client.api.auth.login)["$post"]>;
+type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
+type RequestType = InferRequestType<(typeof client.api.auth.logout)["$post"]>;
 
 export const useLogout = () => {
-  const mutation = useMutation<ResponseType, Error, RequestType>({
-    mutationFn: async ({ json }) => {
-      const response = await client.api.auth.login["$post"]({ json });
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.auth.logout["$post"]();
       return await response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["current"] });
     },
   });
 
