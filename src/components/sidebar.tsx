71168af5d4d73f008f6fe23381c79fa68566cddb
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-10-25 16:55:59 +0530

    sidebar components and some changes

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import DottedSeparator from "./dotted-separator";
+import { Navigation } from "./navigation";
+
+const Sidebar = () => {
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/logo.svg"
+          width={50}
+          height={55}
+          alt="Flowz"
+          draggable="false"
+        />
+        <h1 className="font-semibold text-xl select-none">Flowz</h1>
+      </Link>
+      <DottedSeparator className="my-4" />
+      <Navigation />
+    </aside>
+  );
+};
+
+export default Sidebar;
+
+// 3:57
