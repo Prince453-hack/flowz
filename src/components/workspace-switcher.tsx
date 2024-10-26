b157cecfcc2bd4eed0f64c6429e078bd72a19204
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-10-26 21:09:25 +0530

    modal opening and some ui

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspace";
-import React from "react";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
 import { RiAddCircleFill } from "react-icons/ri";
 import {
   Select,
@@ -10,10 +12,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
-import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
 const WorkspaceSwitcher = () => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data: workspaces } = useGetWorkspaces();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -21,7 +28,7 @@ const WorkspaceSwitcher = () => {
         <p className="text-sm uppercase text-neutral-500">Workspaces</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder="No workspace selected" />
         </SelectTrigger>
