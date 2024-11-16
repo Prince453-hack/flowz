6332dab65b836267ff813c170edf0f25b1909f1e
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-11-16 16:08:45 +0530

    some refactoring

--- src/features/tasks/api/use-update-task.ts
+++ src/features/tasks/api/use-update-task.ts
@@ -1,7 +1,6 @@
 import { client } from "@/lib/rpc";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 type ResponseType = InferResponseType<
@@ -14,7 +13,6 @@ type RequestType = InferRequestType<
 
 export const useUpdateTask = () => {
   const queryClient = useQueryClient();
-  const router = useRouter();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json, param }) => {
@@ -29,7 +27,6 @@ export const useUpdateTask = () => {
     },
     onSuccess: ({ data }) => {
       toast.success("Task updated successfully");
-      router.refresh();
       queryClient.invalidateQueries({ queryKey: ["tasks"] });
       queryClient.invalidateQueries({ queryKey: ["task", data.$id] });
     },
