a4f96bf8887acf77933013b5a28718ffa772e658
Adnan <tabishnizami456@gmail.com>
2025-07-05 23:23:27 +0530

    date calender and task view switcher completed

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ export default {
   experimental: {
     serverActions: true,
   },
-  // env: {
-  //   REST_API_URL: process.env.REST_API_URL,
-  // },
+  env: {
+    REST_API_URL: process.env.REST_API_URL,
+  },
 };
